ccb57a5be5b14757799c2e90d814f1bb4c7e9597
Francesc Busquets <francesc@gmail.com>
2018-03-14 09:52:04 +0100

    Generate all-words.txt when exporting

--- cli/export-project.js
+++ cli/export-project.js
@@ -9,8 +9,20 @@ const readFile = Promise.denodeify(fs.readFile)
 const writeFile = Promise.denodeify(fs.writeFile)
 const glob = Promise.denodeify(require('glob'))
 const parseXml = Promise.denodeify(require('xml2js').parseString)
+const Inspector = require('./inspector-jquery.js')
 
-let jclicPrjBase = null, jclicExportBase = null, exportBase = null, prjName = null, errMsg = ''
+const
+  JCLIC_FOLDER = 'jclic',
+  JS_FOLDER = 'jclic.js',
+  ALLWORDS_FILE = 'all-words.txt',
+  PROJECT_FILE = 'project.json'
+
+let
+  jclicSrc = null,
+  exportSrc = null,
+  exportBase = null,
+  prjName = null,
+  errMsg = ''
 
 function errHandler(err) {
   console.log(err)
@@ -19,15 +31,15 @@ function errHandler(err) {
 
 // Check parameters
 if (process.argv && process.argv.length > 5) {
-  jclicPrjBase = path.resolve(process.cwd(), process.argv[2])
-  jclicExportBase = path.resolve(process.cwd(), process.argv[3])
+  jclicSrc = path.resolve(process.cwd(), process.argv[2])
+  exportSrc = path.resolve(process.cwd(), process.argv[3])
   exportBase = path.resolve(process.cwd(), process.argv[4])
   prjName = process.argv[5]
 
-  if (!fs.lstatSync(jclicPrjBase).isDirectory())
-    errMsg = `ERROR: Directory ${jclicPrjBase} does not exist.`
-  else if (!fs.lstatSync(jclicExportBase).isDirectory())
-    errMsg = `ERROR: Directory ${jclicExportBase} does not exist.`
+  if (!fs.lstatSync(jclicSrc).isDirectory())
+    errMsg = `ERROR: Directory ${jclicSrc} does not exist.`
+  else if (!fs.lstatSync(exportSrc).isDirectory())
+    errMsg = `ERROR: Directory ${exportSrc} does not exist.`
   else if (!fs.lstatSync(exportBase).isDirectory())
     errMsg = `ERROR: Directory ${exportBase} does not exist.`
   else {
@@ -45,20 +57,24 @@ if (process.argv && process.argv.length > 5) {
   if (errMsg)
     errHandler(errMsg)
 } else
-  errHandler(`Usage:\nexport-project [jclicProjectPath] [jclicExportPath] [exportBasePath] [projectName]`)
+  errHandler(`Usage:\nexport-project [jclicSrcDir] [exportSrcDir] [exportBase] [projectName]`)
 
+const
+  jclicDir = path.resolve(exportBase, JCLIC_FOLDER),
+  jclicJsDir = path.resolve(exportBase, JS_FOLDER),
+  projectFile = path.resolve(exportBase, PROJECT_FILE)
 
-console.log(`Copying files...`)
-const jclicDir = path.resolve(exportBase, 'jclic')
-const jclicJsDir = path.resolve(exportBase, 'jclic.js')
 let project = {}
-ncp(jclicPrjBase, jclicDir, errHandler)
-  .then(() => ncp(jclicExportBase, jclicJsDir, errHandler))
-  .then(() => rename(path.resolve(jclicJsDir, 'project.json'), path.resolve(exportBase, 'project.json')))
-  .then(() => readFile(path.resolve(exportBase, 'project.json')))
+
+console.log(`Processing ${prjName}...`)
+
+ncp(jclicSrc, jclicDir, errHandler)
+  .then(() => ncp(exportSrc, jclicJsDir, errHandler))
+  .then(() => rename(path.resolve(jclicJsDir, PROJECT_FILE), projectFile))
+  .then(() => readFile(projectFile))
   .then((data) => {
     project = JSON.parse(data)
-    const moved = ['project.json']
+    const moved = [PROJECT_FILE]
     const promises = []
     if (project.cover) {
       promises.push(rename(path.resolve(jclicJsDir, project.cover), path.resolve(exportBase, project.cover)))
@@ -68,8 +84,11 @@ ncp(jclicPrjBase, jclicDir, errHandler)
       promises.push(rename(path.resolve(jclicJsDir, project.thumbnail), path.resolve(exportBase, project.thumbnail)))
       moved.push(project.thumbnail)
     }
-    project.files = project.files.map(file => moved.includes(file) ? file : `jclic.js/${file}`).sort()
+    project.mainFile = `${JS_FOLDER}/${project.mainFile}`
+    project.files = project.files.map(file => moved.includes(file) ? file : `${JS_FOLDER}/${file}`).sort()
 
+    const mainDir = path.dirname(path.resolve(exportBase, project.mainFile))
+    const inspector = new Inspector()
     promises.push(
       glob(path.resolve(jclicDir, '*.jclic.inst'))
         .then(files => {
@@ -81,19 +100,21 @@ ncp(jclicPrjBase, jclicDir, errHandler)
               .then(str => parseXml(str))
               .then(data => {
                 project.zipFile = path.relative(exportBase, path.resolve(jclicDir, data.JClicInstall.shortcut[0].$.project))
+                // Put `files` at the end of `project`
                 const files = project.files
                 delete project.files
                 project.files = files
               })
           }
+        }),
+      inspector.addProjectsFromPath(mainDir)
+        .then(inspector => {
+          const allWords = inspector.getAllWords().join(' ')
+          return writeFile(path.resolve(exportBase, ALLWORDS_FILE), allWords)
         })
     )
-
     return Promise.all(promises)
   })
-  .then(() => writeFile(path.resolve(exportBase, 'project.json'), JSON.stringify(project, null, 2)))
+  .then(() => writeFile(projectFile, JSON.stringify(project, null, 2)))
   .then(() => console.log(`Done!`))
   .catch(errHandler)
-
-
-
