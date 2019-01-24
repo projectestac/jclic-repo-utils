c69082b2c001a882c74785519b75df2fa52762b1
Francesc Busquets <francesc@gmail.com>
2019-01-24 12:26:37 +0100

    Updated patchInstFiles

--- cli/patchInstFiles.js
+++ cli/patchInstFiles.js
@@ -12,51 +12,88 @@
 
 const fs = require('fs');
 const path = require('path');
-const fileLineExp = /<file src="/;
+const util = require('util');
+const chalk = require('chalk');
 
-const usage = [
-  'Usage:',
-  '  node patchInstFiles.js [path_to_jclic_inst_file]...',
-  '',
-  'Scans the folders for ".jclic.inst" files, detecting missing references.'
-].join('\n');
+const readDir = util.promisify(fs.readdir);
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+const exists = util.promisify(fs.exists);
+const stat = util.promisify(fs.stat);
+const log = console.log;
+
+const fileLineExp = /<file src="/;
 
 const args = process.argv;
 
 if (args.length < 3) {
-  console.error(usage)
-  process.exit(1)
+  log(chalk`
+{bold.blue patchInstFiles v 1.0}
+
+Scans folder for {italic ".jclic.inst"} files, detecting missing references.
+
+Usage:
+  {bold node patchInstFiles.js [path_to_jclic_project]}
+
+`);
+  process.exit(1);
 }
 
-const files = args.slice(2);
+const pathToExplore = args[2];
 
-function processFile(file) {
-  console.log(`Processing ${file}...`);
+function processInstFile(file) {
+
+  log(chalk`{green INFO}: Processing ${file}`);
   const fileName = path.basename(file);
   const base = path.dirname(file);
-  const lines = fs.readFileSync(file).toString().split(/[\n\r]/).filter(s => s.length > 0);
 
-  const fileLines = lines.filter(l => fileLineExp.test(l));
-  fileLines.forEach(line => {
-    const fName = line.match(/"(.*)"/)[1];
-    if (!fs.existsSync(path.resolve(base, fName)))
-      console.log(`WARNING: File ${fName} not found in ${file}`);
-  });
-
-  if (lines.findIndex(l => l.indexOf(fileName) > 0) < 0) {
-    console.log(`File ${file} not auto-referenced. Correcting mistake.`);
-    const firstFileIndex = lines.findIndex(l => fileLineExp.test(l));
-    if (firstFileIndex < 0) {
-      console.log(`ERROR: No 'files' section in ${file}!`);
-      throw ('Error!');
-    }
-    const newLine = lines[firstFileIndex].replace(/"[^"]*"/, `"${fileName}"`);
-    lines.splice(firstFileIndex, 0, newLine);
-    fs.writeFileSync(file, lines.join('\r\n'));
-  }
+  return readFile(file)
+    .then(data => {
+      const lines = data.toString().split(/[\n\r]/).filter(s => s.length > 0);
+      const fileLines = lines.filter(l => fileLineExp.test(l));
+      return Promise.all(fileLines.map(line => {
+        const fName = line.match(/"(.*)"/)[1];
+        return exists(path.resolve(base, fName))
+          .then(result => {
+            if (!result)
+              log(chalk`{redBright.bold WARNING}: File ${fName} not found in ${file}`);
+          });
+      }))
+        .then(() => {
+          if (lines.findIndex(l => l.indexOf(fileName) > 0) < 0) {
+            log(chalk`{yellowBright.bold WARNING}: File ${file} not auto-referenced. Correcting mistake.`);
+            const firstFileIndex = lines.findIndex(l => fileLineExp.test(l));
+            if (firstFileIndex < 0) {
+              console.log(chalk`{redBright.bold ERROR}: No 'files' section in ${file}!`);
+              throw ('Error!');
+            }
+            const newLine = lines[firstFileIndex].replace(/"[^"]*"/, `"${fileName}"`);
+            lines.splice(firstFileIndex, 0, newLine);
+            return writeFile(file, lines.join('\r\n'));
+          }
+        });
+    });
 }
 
-files.forEach(processFile);
+function iterateDir(dir) {
+  return readDir(dir)
+    .then(list => {
+      const instFile = list.find(f => f.match(/\.jclic\.inst$/));
+      if (instFile)
+        return processInstFile(path.join(dir, instFile));
+      else
+        return Promise.all(list.map(file => {
+          const fp = path.join(dir, file);
+          return stat(fp)
+            .then(st => st.isDirectory() ? iterateDir(fp) : Promise.resolve(false));
+        }));
+    })
+    .catch(err => {
+      log(`{redBright.bold ERROR}: Error parsing "${dir}": ${err}`);
+    });
+}
 
-console.log(`Done!`);
+iterateDir(pathToExplore)
+  .then(() => log(chalk`{green INFO}: Done!`))
+  .catch(err => log(`{redBright.bold ERROR}: ${err}`));
 
